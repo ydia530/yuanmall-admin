470a969730bcfe237ef8210d5b545d8b9daa98aa
ydia530 <ydia530@aucklanduni.ac.nz>
2022-01-12 09:03:33 +0800

    Initial commit

---
+++ src/views/common/home.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="mod-home">
+    <div>
+      <h1 class="HeaderClass">项目介绍</h1>
+      <h2>
+        新 谷粒商城项目是一个较大型的分布式电商项目，主要后端采用 Spirngboot 2.3.1.RELEASE，前端使用Vue。
+        前后端简单crud代码均由人人开源的自动生成器生成，学习只用关心较难的业务逻辑。是一个较为不错的用来进阶学习springboot的电商项目。
+      </h2>
+    </div>
+    <div>
+      <h2 class="mainClass">
+        本前端项目是采用人人开源的前端自动生成代码进行修改
+        还有很多未完善的，欢迎大家一同完善。
+      </h2>
+    </div>
+    <div>
+      <h2 class="footerClass"> 如有问题请联系 QQ: 2465652971</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style>
+  .mod-home {
+    line-height: 2;
+  }
+
+  .HeaderClass {
+    font-size: 30px;
+    color: indianred;
+  }
+
+  .mainClass {
+    font-size: 25px;
+    color: green;
+  }
+
+  .footerClass {
+    font-size: 25px;
+    color: blue;
+  }
+</style>
+
